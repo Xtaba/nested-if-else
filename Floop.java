97aec725ec562906d6c043acd85dff7c9a5d3ef8
Xtaba <ehsunt@yahoo.com>
2017-01-15 14:26:33 -0800

    Add files via upload

---
+++ Floop.java
@@ -0,0 +1,11 @@
+//for loop
+
+
+public class Floop {
+	public static void main(String[] args){
+		for(int counter =6; counter <= 21; counter+=3){//takes 3 argument inside it,first where you want it to start,second where you want it to end, how much increament it by
+			System.out.println(counter); //starts at one until it hits 10 and printed it 
+		}
+	
+	}
+}
