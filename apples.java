84fb42d2a322e5637d1b8fb83dca6e5a24218354
Xtaba <ehsunt@yahoo.com>
2017-01-15 13:12:00 -0800

    Add files via upload

---
+++ apples.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class apples {
+	public static void main(String[] args){
+		int age = 60;
+		 
+		if (age < 50){
+			System.out.println("You are Young");
+		}else {
+			System.out.println("You are OLD");
+			if (age > 75){
+				System.out.println("You are really old");
+			}else {
+				System.out.println("Don't worry not too old");
+			}
+		}
+	}
+
+}
