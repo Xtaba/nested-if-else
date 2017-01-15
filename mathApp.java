713361c63a4155bfab7a1cb92acd6b917bfa1a6b
Xtaba <ehsunt@yahoo.com>
2017-01-15 15:23:56 -0800

    Add files via upload

---
+++ mathApp.java
@@ -0,0 +1,13 @@
+//methods in math class
+
+public class mathApp {
+	public static void main(String[] args){
+		System.out.println(Math.abs(-26.7)); //math functions
+		System.out.println(Math.ceil(7.4)); //rounds up
+		System.out.println(Math.floor(7.4)); //rounds down
+		System.out.println(Math.max(344.2,223.4)); //gives you the max of two number
+		System.out.println(Math.min(6.7, -54.4)); // gives you the min of two numbers
+		System.out.println(Math.pow(5,3)); //5 first argument 3 is power
+		System.out.println(Math.sqrt(9)); //finds the square root of a number
+	}
+}
