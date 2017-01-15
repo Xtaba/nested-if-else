8c7ad62d097669e45bb4666cdd456d6de77452de
Xtaba <ehsunt@yahoo.com>
2017-01-15 13:22:59 -0800

    Add files via upload

---
+++ apples2.java
@@ -0,0 +1,11 @@
+//Conditional Operators: another way of using if else statement
+//much more compact and small
+public class apples2 {
+	public static void main(String[] args){
+		int age = 21;
+		
+		System.out.println(age > 50 ? "You are old" : "You are young"); //treat it as a boolean expression if it's true prints the first "..." if not the second "..."
+	
+	}
+
+}
