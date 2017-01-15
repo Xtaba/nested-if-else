0f6d25032f571d04867f1d3bc561c7650ded75cb
Xtaba <ehsunt@yahoo.com>
2017-01-15 15:41:06 -0800

    Add files via upload

---
+++ arrayInit.java
@@ -0,0 +1,12 @@
+//array is a single variable that can hold multiple variables in it
+import java.util.Random;
+
+public class arrayInit {
+	public static void main(String[] args){
+		
+		int Ehsun[]={2,4,5,7,9}; //array initializers, automatically give value to each Ehsun
+		
+		System.out.println(Ehsun[4]);
+		
+	}
+}
