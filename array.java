9d3798ee9f786519401cd232ea4cd43c181015d0
Xtaba <ehsunt@yahoo.com>
2017-01-15 15:38:31 -0800

    Add files via upload

---
+++ array.java
@@ -0,0 +1,17 @@
+//array is a single variable that can hold multiple variables in it
+import java.util.Random;
+
+public class array {
+	public static void main(String[] args){
+		int Ehsun[] = new int [10]; //it can store 10 variables in a single array
+		
+		Ehsun[0]= 95; //each index allows us to store different variables
+		Ehsun[1]=2;
+		Ehsun[3]=333;
+		Ehsun[4]=12;
+		Ehsun[5]=32;
+		Ehsun[9]= 80;
+		
+		System.out.println(Ehsun[1]);
+	}
+}
