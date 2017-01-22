afe02be29bb5e83669332f83b4601fad11aa7f13
Xtaba <ehsunt@yahoo.com>
2017-01-22 15:02:04 -0800

    Add files via upload

---
+++ constructor3.java
@@ -0,0 +1,13 @@
+//two string method
+ //has a range method in defined in it
+
+
+public class constructor {
+	public static void main(String[] args){
+		tuna member1 = new tuna("Megan", "fox");
+		tuna member2 = new tuna("NAatakie", "portman");
+		tuna member3 = new tuna("Taylor", "swift"); //three diff object, each has its own set of variables
+	}
+}
+
+//Object, description, year
---
+++ tuna3.java
@@ -0,0 +1,16 @@
+//enumeration or enum is like classes to declare constants
+public class tuna {
+	private String first;
+	private String last;
+	private static int members = 0;
+	
+	public tuna(String fn, String ln){
+		first = fn;
+		last = ln;
+		members++;
+		System.out.printf("Construsctor for %s %s, members in the club: %d\n", first, last, members);
+		
+	}
+	
+}
+
